6f3c23d1cf8e71490107cdfa3f42438ade4aa34f
David Olivari <david.olivari@gmail.com>
2024-02-01 16:58:24 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,66 @@
 <script>
-  import './fonts/stylesheet.css'
+  import '../assets/fonts/stylesheet.css'
+  import { Canvas, T } from '@threlte/core'
+  import { OrbitControls } from '@threlte/extras'
   import logo from '../assets/full_logo.svg'
+  let position = [-8, 80, -2]
+  let rotation = [-1.6, -0.1, 0]
 </script>
 
-<center>
-  <img src={logo} alt="logo" class="logo" width="30%" />
-</center>
-
-<div class="big">Production de dispositifs</div>
-
-<div class="quoteblock">
-  <p class="quote clip-text clip-text_one">
-    «J'appelle dispositif tout ce qui a, d'une manière ou d'une autre, la
-    capacité de capturer, d'orienter, de déterminer, d'intercepter, de modeler,
-    de contrôler, et d'assurer les gestes, les conduites, les opinions et les
-    discours des êtres vivants»
-  </p>
-  <small>
-    <p class="quoteref">
-      Qu'est-ce qu'un dispositif ?
-      <br />
-      Giorgio Agamben
-    </p>
-  </small>
-</div>
+<img src={logo} alt="logo" class="logo" width="30%" />
+<p>
+  l'entreprise <b>bureau d'études artistiques et technique</b>
+  propose des prestations d'accompagnement à l'innovation et la production de projets
+  dans le domaine artistique et industrielle. de la conception à la livraison nous
+  offrons nos compétences en matière d'ingénierie, design, interopérabilité, fabrication
+  et supervision.
+</p>
+<p>
+  les méthodes agiles sont au cœur de notre gestion de projet, elles permettent
+  une traçabilité et assurent un niveau de qualité quelque soit la taille du
+  projet. une veille permanente nous permet d'être au plus proche des standards
+  émergents et technologies innovantes.
+</p>
+<p>
+  la société a été fondée par <b>David Olivari</b>
+  ingénieur de formation, il a participé à des entreprises dans le domaine du calcul
+  scientifique appliqué aux sciences du vivant en 1995, puis de la télévision interactive
+  en 2000 pour devenir indépendant en 2004 et commencer à accompagner techniquement
+  des projets artistiques de plus en plus complexes. parallèlement à ces activités
+  il a également effectué des missions pour des entreprises industrielles et réalisé
+  plusieurs dispositifs critiques dans le domaine de l'énergie, de la mesure scientifique
+  et du diagnostic médical.
+</p>
+<p>
+  en alliant les exigences qualitatives des standards de l'industrie et la
+  créativité propre aux projets artistiques, il a pu mener à bien des
+  interventions sur des événements aussi important que les jeux olympiques de
+  Londres en 2012, la COP21 de Paris en 2015 et le suivi de 3 millions de tests
+  covid en 2020.
+</p>
+<p>
+  la création de <b>bé.at</b>
+  est le fruit d'une réflexion vers plus d'indépendance et d'autonomie autour des
+  valeurs de qualité, fiabilité et créativité.
+</p>
+<p>
+  Contactez nous : <a href="mailto:c@bé.at">c@bé.at</a>
+</p>
 
 <style>
   :global(body) {
     background-color: rgb(241, 241, 225);
     color: black;
     font-family: 'Alte Haas Grotesk', sans-serif;
+    font-size: 12pt;
     margin: 0;
-    padding: 4vw 0 0 0;
+    padding: 5vw;
   }
-  .big {
-    font-size: 6.5vw;
-    line-height: 5vw;
-    font-weight: bold;
-    text-align: center;
-    text-transform: uppercase;
-    padding: 10% 0;
-    white-space: nowrap;
+  :global(a) {
+    color: black;
+    text-decoration: dotted underline;
   }
-  .quote {
-    font-size: 3vw;
-    font-style: italic;
-    text-align: left;
-  }
-  .quoteblock {
-    color: white;
-    padding: 5% 10% 5%;
-    background-color: orangered;
-  }
-  .quoteref {
-    color: white;
-    font-size: 2vw;
-    font-style: normal;
-    text-align: right;
+  p {
+    max-width: 960px;
   }
 </style>
